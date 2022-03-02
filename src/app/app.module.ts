21636252b2e6e64839c57a78c6c406917ef40c53
xgaitan <xiomiliz.99@gmail.com>
2022-03-01 22:43:34 -0500

    prueba1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
-/* new form imports */
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
@@ -12,10 +10,7 @@ import {AppComponent} from "./app.component";
   declarations: [AppComponent],
   imports: [BrowserModule,
     AmplifyAuthenticatorModule,
-    AppRoutingModule,
-   /* configuring form modules */
-    FormsModule,
-    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
